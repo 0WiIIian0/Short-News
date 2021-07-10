5bab195c0e77679d13407a15bc07c96c892b58cf
Renault Scabora <renaultscabora@hotmail.com>
2021-07-10 00:50:44 -0300

    Add DB and Back-End/User

---
+++ back-end/db/shortnews.sql
@@ -0,0 +1,183 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 10-Jul-2021 às 04:38
+-- Versão do servidor: 10.4.19-MariaDB
+-- versão do PHP: 7.4.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `shortnews`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `text` int(11) NOT NULL,
+  `reactions` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`reactions`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `subtitle` text NOT NULL,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`content`)),
+  `category` tinytext NOT NULL,
+  `reactions` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`reactions`)),
+  `post_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `reported_comments`
+--
+
+CREATE TABLE `reported_comments` (
+  `id` int(11) NOT NULL,
+  `comment_id` int(11) NOT NULL,
+  `complaint` text NOT NULL,
+  `reported_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `reported_news`
+--
+
+CREATE TABLE `reported_news` (
+  `id` int(11) NOT NULL,
+  `news_id` int(11) NOT NULL,
+  `complaint` text NOT NULL,
+  `reported_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `email` text NOT NULL,
+  `pass` text NOT NULL,
+  `permission_type` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `news_user_id` (`user_id`) USING BTREE;
+
+--
+-- Índices para tabela `reported_comments`
+--
+ALTER TABLE `reported_comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `reported_news`
+--
+ALTER TABLE `reported_news`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `reported_news_id` (`news_id`);
+
+--
+-- Índices para tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `reported_comments`
+--
+ALTER TABLE `reported_comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `reported_news`
+--
+ALTER TABLE `reported_news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `news`
+--
+ALTER TABLE `news`
+  ADD CONSTRAINT `id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Limitadores para a tabela `reported_news`
+--
+ALTER TABLE `reported_news`
+  ADD CONSTRAINT `reported_news_id` FOREIGN KEY (`news_id`) REFERENCES `news` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ back-end/user/User.php
@@ -0,0 +1,38 @@
+<?php
+
+class User {
+
+    private $id = '';
+    private $name = '';
+    private $email = '';
+
+    function __construct(
+        $id,
+        $name,
+        $email) {
+        
+    }
+
+    function delete() {
+
+    }
+
+    function changeName() {
+
+    }
+
+    function changePassword() {
+        
+    }
+
+    function changePermission() {
+        
+    }
+
+    function checkPassword($receivedPassword) {
+
+    }
+
+};
+
+?>
