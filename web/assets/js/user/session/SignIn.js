d2917590d463deab31263e4f62f9395904dd2889
Renaultivo <renaultscabora@hotmail.com>
2021-07-12 01:33:25 -0300

    Feat(SessionMenu) add SessionMenu design

--- web/assets/js/user/session/SignIn.js
+++ web/assets/js/user/session/SignIn.js
@@ -1,8 +1,38 @@
 export default function SignIn() {
 
     return createElement({
-        class: 'fragment flexBoxAlign',
-        content: 'Sign-In'
+        tag: 'form',
+        class: 'fragment flexBoxAlign columnDirection',
+        content: [
+            createElement({
+                class: 'title',
+                content: 'Sign-In'
+            }),
+            createElement({
+                class: 'flexBoxAlign columnDirection',
+                content: [
+                    createElement({
+                        tag: 'input',
+                        attributes: {
+                            type: 'email',
+                            placeholder: 'Email'
+                        }
+                    }),
+                    createElement({
+                        tag: 'input',
+                        attributes: {
+                            type: 'password',
+                            placeholder: 'Password'
+                        }
+                    }),
+                    createElement({
+                        class: 'button hoverGrow',
+                        ripple: '#000000',
+                        content: 'Enter'
+                    })
+                ]
+            })
+        ]
     });
 
 }
--- web/assets/js/user/session/SignUp.js
+++ web/assets/js/user/session/SignUp.js
@@ -1,8 +1,45 @@
 export default function SignIn() {
 
     return createElement({
-        class: 'fragment flexBoxAlign',
-        content: 'Sign-Up'
+        tag: 'form',
+        class: 'fragment flexBoxAlign columnDirection',
+        content: [
+            createElement({
+                class: 'title',
+                content: 'Sign-Up'
+            }),
+            createElement({
+                class: 'flexBoxAlign columnDirection',
+                content: [
+                    createElement({
+                        tag: 'input',
+                        attributes: {
+                            type: 'text',
+                            placeholder: 'Username'
+                        }
+                    }),
+                    createElement({
+                        tag: 'input',
+                        attributes: {
+                            type: 'email',
+                            placeholder: 'Email'
+                        }
+                    }),
+                    createElement({
+                        tag: 'input',
+                        attributes: {
+                            type: 'password',
+                            placeholder: 'Password'
+                        }
+                    }),
+                    createElement({
+                        class: 'button hoverGrow',
+                        ripple: '#000000',
+                        content: 'Enter'
+                    })
+                ]
+            })
+        ]
     });
 
 }
